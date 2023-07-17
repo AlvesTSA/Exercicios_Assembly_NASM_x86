67413700f12979c9678e186cdcba8def33ba64c6
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-07-17 17:46:55 -0300

    exercicio_1

---
+++ Estrutura_Sequencial/exercicio_001.asm
@@ -0,0 +1,25 @@
+; 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+section .data
+        
+    msg db 'Olá, mundo !', 0x0A     ; 10 é o código de escape para '\n'
+    len equ $ - msg                 ; contagem automática de caracteres da msg
+
+section .text
+
+    global _start  ; nome do rótulo para dá início ao programa 
+
+_start:
+
+    ;Chamada sys_write para saida padrão de dados
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
+    syscall
+
+    ;Chamada sys_exit para fechamento do programa
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
